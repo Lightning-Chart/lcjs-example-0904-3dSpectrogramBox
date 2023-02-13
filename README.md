e4e1d1f834816324a598cf730497412e82b418e2
Ilkka Kuosmanen <ilkka.kuosmanen@lightningchart.com>
2023-02-13 13:48:31 +0200

    LCJS 4.0.1

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # JavaScript Spectrogram Box Series 3D
 
-![JavaScript Spectrogram Box Series 3D](3dSpectrogramBox.png)
+![JavaScript Spectrogram Box Series 3D](3dSpectrogramBox-darkGold.png)
 
 This demo application belongs to the set of examples for LightningChart JS, data visualization library for JavaScript.
 
@@ -8,22 +8,22 @@ LightningChart JS is entirely GPU accelerated and performance optimized charting
 
 The demo can be used as an example or a seed project. Local execution requires the following steps:
 
-- Make sure that relevant version of [Node.js](https://nodejs.org/en/download/) is installed
-- Open the project folder in a terminal:
+-   Make sure that relevant version of [Node.js](https://nodejs.org/en/download/) is installed
+-   Open the project folder in a terminal:
 
-        npm install              # fetches dependencies
-        npm start                # builds an application and starts the development server
+          npm install              # fetches dependencies
+          npm start                # builds an application and starts the development server
 
-- The application is available at *http://localhost:8080* in your browser, webpack-dev-server provides hot reload functionality.
+-   The application is available at _http://localhost:8080_ in your browser, webpack-dev-server provides hot reload functionality.
 
 
 ## Description
 
-This example shows a use case for 3D Box Series as a 3D *spectrogram*
+This example shows a use case for 3D Box Series as a 3D _spectrogram_
 
 Spectrogram is a visual representation of the spectrum of frequencies. Spectrograms can be used to visualize any wave form. Most often spectrograms are used to display audio signals.
 
-The spectrogram shows frequency on one axis (X Axis) and time on the other (Z Axis). The amplitude of the frequency is displayed both by the Y coordinate as well as the dynamically applied *color*.
+The spectrogram shows frequency on one axis (X Axis) and time on the other (Z Axis). The amplitude of the frequency is displayed both by the Y coordinate as well as the dynamically applied _color_.
 
 
 ## API Links
@@ -38,25 +38,25 @@ The spectrogram shows frequency on one axis (X Axis) and time on the other (Z Ax
 
 If you notice an error in the example code, please open an issue on [GitHub][0] repository of the entire example.
 
-Official [API documentation][1] can be found on [Arction][2] website.
+Official [API documentation][1] can be found on [LightningChart][2] website.
 
 If the docs and other materials do not solve your problem as well as implementation help is needed, ask on [StackOverflow][3] (tagged lightningchart).
 
-If you think you found a bug in the LightningChart JavaScript library, please contact support@arction.com.
+If you think you found a bug in the LightningChart JavaScript library, please contact support@lightningchart.com.
 
-Direct developer email support can be purchased through a [Support Plan][4] or by contacting sales@arction.com.
+Direct developer email support can be purchased through a [Support Plan][4] or by contacting sales@lightningchart.com.
 
 [0]: https://github.com/Arction/
-[1]: https://www.arction.com/lightningchart-js-api-documentation/
-[2]: https://www.arction.com
+[1]: https://lightningchart.com/lightningchart-js-api-documentation/
+[2]: https://lightningchart.com
 [3]: https://stackoverflow.com/questions/tagged/lightningchart
-[4]: https://www.arction.com/support-services/
+[4]: https://lightningchart.com/support-services/
 
-© Arction Ltd 2009-2020. All rights reserved.
+© LightningChart Ltd 2009-2022. All rights reserved.
 
 
-[3D chart]: https://www.arction.com/lightningchart-js-api-documentation/v3.4.0/classes/chart3d.html
-[3D box series]: https://www.arction.com/lightningchart-js-api-documentation/v3.4.0/classes/boxseries3d.html
-[Paletted Fill Style]: https://www.arction.com/lightningchart-js-api-documentation/v3.4.0/classes/palettedfill.html
-[LUT]: https://www.arction.com/lightningchart-js-api-documentation/v3.4.0/classes/lut.html
+[3D chart]: https://lightningchart.com/lightningchart-js-api-documentation/v4.0.0/classes/Chart3D.html
+[3D box series]: https://lightningchart.com/lightningchart-js-api-documentation/v4.0.0/classes/BoxSeries3D.html
+[Paletted Fill Style]: https://lightningchart.com/lightningchart-js-api-documentation/v4.0.0/classes/PalettedFill.html
+[LUT]: https://lightningchart.com/lightningchart-js-api-documentation/v4.0.0/classes/LUT.html
 
--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@
 // Import LightningChartJS
 const lcjs = require('@arction/lcjs')
 
+// Import xydata
+const xydata = require('@arction/xydata')
+
 // Extract required parts from LightningChartJS.
 const {
     lightningChart,
@@ -15,12 +18,11 @@ const {
     UIOrigins,
     UILayoutBuilders,
     UIBackgrounds,
-    Themes
+    regularColorSteps,
+    Themes,
 } = lcjs
 
-const {
-    createSpectrumDataGenerator
-} = require('@arction/xydata')
+const { createSpectrumDataGenerator } = xydata
 
 // Length of single data sample.
 const dataSampleSize = 60
@@ -29,182 +31,164 @@ const dataSampleSize = 60
 const dataHistoryLength = 120
 
 // Initial camera location.
-const initialCameraLocation = {x: 0.10, y: 0.08, z: 1.2}
-
-
-// Setup PalettedFill for dynamically coloring Boxes by an associated 'value' property.
-const lut = new LUT( {
-    steps: [
-        { value: 15, color: ColorRGBA( 0, 0, 0 ) },
-        { value: 30, color: ColorRGBA( 255, 255, 0 ) },
-        { value: 45, color: ColorRGBA( 255, 204, 0 ) },
-        { value: 60, color: ColorRGBA( 255, 128, 0 ) },
-        { value: 100, color: ColorRGBA( 255, 0, 0 ) }
-    ],
-    units: 'dB',
-    interpolate: true
-} )
-const paletteFill = new PalettedFill( { lut, lookUpProperty: 'y' } )
-
+const initialCameraLocation = { x: 0.1, y: 0.08, z: 1.2 }
 
 // Create Chart3D and configure Axes.
-const chart3D = lightningChart().Chart3D({
-    disableAnimations: true,
-    // theme: Themes.darkGold
-})
-    .setTitle( '3D Box Series Spectrogram' )
-    .setBoundingBox( { x: 1, y: 1, z: 2 } )
-chart3D.setCameraLocation( initialCameraLocation )
-chart3D.getDefaultAxisY()
-    .setScrollStrategy( AxisScrollStrategies.expansion )
-    .setInterval( 0, 100 )
-    .setTitle( 'Power spectrum P(f)' )
-chart3D.getDefaultAxisX()
-    .setTitle( 'Frequency (Hz)' )
-chart3D.getDefaultAxisZ()
-    .setTitle( 'Time' )
-    .setInterval( 0, -dataHistoryLength )
-    .setScrollStrategy( AxisScrollStrategies.progressive )
+const chart3D = lightningChart()
+    .Chart3D({
+        // theme: Themes.darkGold
+    })
+    .setTitle('3D Box Series Spectrogram')
+    .setBoundingBox({ x: 1, y: 1, z: 2 })
+chart3D.setCameraLocation(initialCameraLocation)
+chart3D
+    .getDefaultAxisY()
+    .setScrollStrategy(AxisScrollStrategies.expansion)
+    .setInterval({ start: 0, end: 100, stopAxisAfter: false })
+    .setTitle('Power spectrum P(f)')
+chart3D.getDefaultAxisX().setTitle('Frequency (Hz)')
+chart3D
+    .getDefaultAxisZ()
+    .setTitle('Time')
+    .setInterval({ start: 0, end: -dataHistoryLength, stopAxisAfter: false })
+    .setScrollStrategy(AxisScrollStrategies.progressive)
 
+// Setup PalettedFill for dynamically coloring Boxes by an associated 'value' property.
+const theme = chart3D.getTheme()
+const lut = new LUT({
+    steps: regularColorSteps(0, 100, theme.examples.intensityColorPalette),
+    units: 'dB',
+    interpolate: true,
+})
+const paletteFill = new PalettedFill({ lut, lookUpProperty: 'y' })
 
 // Create Box Series 3D.
-const boxSeries = chart3D.addBoxSeries()
-    .setFillStyle( paletteFill )
-    .setRoundedEdges( undefined )
-    .setName('Spectrogram (box)')
-
-
+const boxSeries = chart3D.addBoxSeries().setFillStyle(paletteFill).setRoundedEdges(undefined).setName('Spectrogram (box)')
 
 // Create a data grid of Boxes which we can mutate without needing to create new Boxes for best performance.
 const boxGrid = []
-for ( let sampleIndex = 0; sampleIndex < dataHistoryLength; sampleIndex++ ) {
+for (let sampleIndex = 0; sampleIndex < dataHistoryLength; sampleIndex++) {
     const sampleBoxIDs = []
-    for ( let i = 0; i < dataSampleSize; i++ ) {
+    for (let i = 0; i < dataSampleSize; i++) {
         const id = sampleIndex * dataSampleSize + i
         // Add empty Box to series.
-        boxSeries.invalidateData( [{
-            id,
-            yMin: 0,
-            yMax: 0,
-            zMin: 0,
-            zMax: 0,
-            // Box X coordinates don't have to change afterwards.
-            xMin: i,
-            xMax: i + 1.0
-        }] )
-        sampleBoxIDs.push( id )
+        boxSeries.invalidateData([
+            {
+                id,
+                yMin: 0,
+                yMax: 0,
+                zMin: 0,
+                zMax: 0,
+                // Box X coordinates don't have to change afterwards.
+                xMin: i,
+                xMax: i + 1.0,
+            },
+        ])
+        sampleBoxIDs.push(id)
     }
-    boxGrid.push( sampleBoxIDs )
+    boxGrid.push(sampleBoxIDs)
 }
 
 // Add LegendBox to chart.
-const legend = chart3D.addLegendBox()
+const legend = chart3D
+    .addLegendBox()
     // Dispose example UI elements automatically if they take too much space. This is to avoid bad UI on mobile / etc. devices.
     .setAutoDispose({
         type: 'max-width',
-        maxWidth: 0.30,
+        maxWidth: 0.3,
     })
     .add(chart3D)
 
-
 let sampleIndex = 0
 createSpectrumDataGenerator()
-    .setSampleSize( dataSampleSize )
-    .setNumberOfSamples( dataHistoryLength )
-    .setVariation( 5.0 )
+    .setSampleSize(dataSampleSize)
+    .setNumberOfSamples(dataHistoryLength)
+    .setVariation(5.0)
     .generate()
-    .setStreamRepeat( true )
-    .setStreamInterval( 1000 / 60 )
-    .setStreamBatchSize( 1 )
+    .setStreamRepeat(true)
+    .setStreamInterval(1000 / 60)
+    .setStreamBatchSize(1)
     .toStream()
     // Scale Y values from [0.0, 1.0] to [0.0, 80]
-    .map( sample => sample.map( y => y * 80 ) )
+    .map((sample) => sample.map((y) => y * 80))
     // Map Y values to a Row of Boxes.
-    .forEach( sample => {
+    .forEach((sample) => {
         const infiniteStreamingDataEnabled = toggleStreamingCheckBox.getOn()
         const addSample = infiniteStreamingDataEnabled || sampleIndex < dataHistoryLength
 
-        if ( addSample ) {
+        if (addSample) {
             // Instead of making new Boxes each sample (slow), get the oldest row of Boxes that were created beforehand.
             const rowIndex = sampleIndex % dataHistoryLength
             const boxIDs = boxGrid[rowIndex]
 
             // Map Y values to BoxDataBounds format.
-            const boxesData = sample.map( ( y, i ) => ( {
+            const boxesData = sample.map((y, i) => ({
                 id: boxIDs[i],
                 yMin: 0,
                 yMax: y,
                 zMin: sampleIndex,
                 zMax: sampleIndex + 1.0,
-            } ) )
-            boxSeries.invalidateData( boxesData )
+            }))
+            boxSeries.invalidateData(boxesData)
 
             sampleIndex++
         }
-    } )
-
+    })
 
 // Animate Camera movement from file.
 ;(async () => {
-    const cameraAnimationData = await (
-        fetch( document.head.baseURI + 'examples/assets/0904/camera.json' )
-            .then( r => r.json() )
-    )
-    if ( ! cameraAnimationData ) {
+    const cameraAnimationData = await fetch(document.head.baseURI + 'examples/assets/0904/camera.json').then((r) => r.json())
+    if (!cameraAnimationData) {
         console.log(`No Camera animation data.`)
         return
     }
     console.log(`Loaded Camera animation data.`)
     let frame = 0
     const nextFrame = () => {
-        if ( cameraAnimationEnabledCheckbox.getOn() ) {
+        if (cameraAnimationEnabledCheckbox.getOn()) {
             const { cameraLocation } = cameraAnimationData.frames[Math.floor(frame) % cameraAnimationData.frames.length]
-            chart3D.setCameraLocation( cameraLocation )
+            chart3D.setCameraLocation(cameraLocation)
             frame += 2
         }
-        requestAnimationFrame( nextFrame )
+        requestAnimationFrame(nextFrame)
     }
-    requestAnimationFrame( nextFrame )
+    requestAnimationFrame(nextFrame)
 })()
 
-
-
 // * UI controls *
-const group = chart3D.addUIElement( UILayoutBuilders.Column
-    .setBackground( UIBackgrounds.Rectangle )
-)
+const group = chart3D.addUIElement(UILayoutBuilders.Column.setBackground(UIBackgrounds.Rectangle))
 group
-    .setPosition( { x: 0, y: 100 } )
-    .setOrigin( UIOrigins.LeftTop )
-    .setMargin( 10 )
-    .setPadding( 4 )
+    .setPosition({ x: 0, y: 100 })
+    .setOrigin(UIOrigins.LeftTop)
+    .setMargin(10)
+    .setPadding(4)
     // Dispose example UI elements automatically if they take too much space. This is to avoid bad UI on mobile / etc. devices.
     .setAutoDispose({
         type: 'max-height',
-        maxHeight: 0.30,
+        maxHeight: 0.3,
     })
 
 // Add UI control for toggling between infinite streaming data and static data amount.
-const handleStreamingToggled = ( state ) => {
-    toggleStreamingCheckBox.setText( state ? 'Disable infinite streaming data' : 'Enable infinite streaming data' )
-    if ( toggleStreamingCheckBox.getOn() !== state ) {
-        toggleStreamingCheckBox.setOn( state )
+const handleStreamingToggled = (state) => {
+    toggleStreamingCheckBox.setText(state ? 'Disable infinite streaming data' : 'Enable infinite streaming data')
+    if (toggleStreamingCheckBox.getOn() !== state) {
+        toggleStreamingCheckBox.setOn(state)
     }
 }
-const toggleStreamingCheckBox = group.addElement( UIElementBuilders.CheckBox )
-toggleStreamingCheckBox.onSwitch(( _, state ) => handleStreamingToggled( state ) )
-handleStreamingToggled( true )
+const toggleStreamingCheckBox = group.addElement(UIElementBuilders.CheckBox)
+toggleStreamingCheckBox.onSwitch((_, state) => handleStreamingToggled(state))
+handleStreamingToggled(true)
 
 // Add UI control for toggling camera animation.
-const handleCameraAnimationToggled = ( state ) => {
-    cameraAnimationEnabledCheckbox.setText( state ? 'Disable camera animation' : 'Enable camera animation' )
-    if ( cameraAnimationEnabledCheckbox.getOn() !== state ) {
-        cameraAnimationEnabledCheckbox.setOn( state )
+const handleCameraAnimationToggled = (state) => {
+    cameraAnimationEnabledCheckbox.setText(state ? 'Disable camera animation' : 'Enable camera animation')
+    if (cameraAnimationEnabledCheckbox.getOn() !== state) {
+        cameraAnimationEnabledCheckbox.setOn(state)
     }
 }
-const cameraAnimationEnabledCheckbox = group.addElement( UIElementBuilders.CheckBox )
-cameraAnimationEnabledCheckbox.onSwitch((_, state) => handleCameraAnimationToggled( state ))
-handleCameraAnimationToggled( true )
+const cameraAnimationEnabledCheckbox = group.addElement(UIElementBuilders.CheckBox)
+cameraAnimationEnabledCheckbox.onSwitch((_, state) => handleCameraAnimationToggled(state))
+handleCameraAnimationToggled(true)
 chart3D.onBackgroundMouseDrag(() => {
-    handleCameraAnimationToggled( false )
+    handleCameraAnimationToggled(false)
 })
